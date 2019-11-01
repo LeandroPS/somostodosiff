90870d15dd59e69908095cdc5e21b4dfc4570da3
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2019-11-01 19:05:45 -0300

    adding footer, agenda and links to documents

--- dist/styles.cbe67578.js
+++ dist/styles.cbe67578.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../images/banner.png":[["banner.462977a5.png","assets/images/banner.png"],"assets/images/banner.png"],"_css_loader":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61032" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49199" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
